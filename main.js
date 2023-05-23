54f2d912317e90dd29bf17f0b35a49dfbe1f438a
takeitEasyhwan <wlghks0508@naver.com>
2023-05-24 04:44:25 +0900

    2

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ var green_value = 0;
 var blue_value = 0;
 var bgm1 = new Audio('source/startbgm1.mp3');
 var bgm2 = new Audio('source/startbgm2.mp3');
+var clickSound1 = new Audio('source/click1.mp3')
 
 //선택화면
 var mode;
@@ -89,6 +90,8 @@ $(document).ready(function () {
     $("#startbtn").click(function () {
         $("#start").hide();
         $("#select").fadeIn();
+        bgm1.pause();
+        bgm2.pause();
         // $("#select").css('display','gird');
     });
 
@@ -99,7 +102,7 @@ $(document).ready(function () {
         mode = $(this).index();
         $('#mode_select').hide();
         $('#team_select').fadeIn();
-        play();
+        clickSound1.play();
     });
 
     $('.team').click(function () {
@@ -111,11 +114,11 @@ $(document).ready(function () {
         else {
             //게임시작
         }
-        play();
+        clickSound1.play();
     });
     $('.level').click(function () {
         level = $(this).index();
-        play();
+        clickSound1.play();
     });
 
     function play() {
